301a18433bdd82aa2e3fa85d8946d5eaca0a0987
芦崎恵介 <keisuke@ashisakikeisukenoMacBook-puro.local>
2018-11-05 22:40:24 +0900

    add chapter1 files

---
+++ chapter1/chat/room.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ashizaki/goweb/chapter1/trace"
+	"github.com/gorilla/websocket"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: messageBufferSize,
+}
+
+type room struct {
+	forword chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+	tracer  trace.Tracer
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServerHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() {
+		r.leave <- client
+	}()
+
+	go client.write()
+	client.read()
+
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
+		case msg := <-r.forword:
+			// 全てのクライアントにメッセージを送信
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+					r.tracer.Trace(" -- クライアントに送信されました")
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
+				}
+			}
+		}
+	}
+}
+
+func newRoom() *room {
+	return &room{
+		forword: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
+	}
+}
